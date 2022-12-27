e2e4dc8d40d48a9165e18d0f98ab357763504627
dinhtkien <dinhtkien@gmail.com>
2022-12-27 21:53:02 +0700

    built

--- src/components/Meme.tsx
+++ src/components/Meme.tsx
@@ -14,9 +14,9 @@ export default function Meme(props: MemeProps) {
                     <button className="btn btn-lg w-75 text-light"
                         name="memeButton" onClick={(event) => { props.stateHandler(event) }} >Get A New Meme Image</button>
                 </div>
-                <div className="row d-flex justify-content-center position-relative">
+                <div className="row position-relative">
                     <h2 className="top-meme-text position-absolute">{props.topMemeText}</h2>
-                    <img src={props.url} alt="meme"></img>
+                    <img src={props.url} className="w-50" alt="meme"></img>
                     <h2 className="bottom-meme-text position-absolute">{props.bottomMemeText}</h2>
                 </div>
             </div>
